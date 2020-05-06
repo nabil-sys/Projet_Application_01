fbdec98e8fff70d24f1c25c741ec370cff9a67aa
nabil-sys <boukhenaissi@et.esiea.fr>
2020-05-06 18:32:10 +0200

    [feature][ApiRest] ExerciceImage version API rest

--- app/src/main/java/com/example/projetapplication01/MainActivity.java
+++ app/src/main/java/com/example/projetapplication01/MainActivity.java
@@ -5,19 +5,37 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
+    private static final String BASE_URL = "https://wger.de";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showList();
+        makeApiCall();
+    }
+
+    private void showList(){
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
@@ -31,5 +49,45 @@ public class MainActivity extends AppCompatActivity {
         // define an adapter
         mAdapter = new ListAdapter(input);
         recyclerView.setAdapter(mAdapter);
+    }
+
+    private void makeApiCall(){
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        ExoApi exoApi = retrofit.create(ExoApi.class);
+
+        Call<RestExerciceImageResponse> call = exoApi.getExerciceImageResponse();
+        call.enqueue(new Callback<RestExerciceImageResponse>() {
+            @Override
+            public void onResponse(Call<RestExerciceImageResponse> call, Response<RestExerciceImageResponse> response) {
+                if(response.isSuccessful() && response.body() != null){
+                    List<ExerciceImage> exerciceImageList = response.body().getResults();
+                    Toast.makeText(getApplicationContext(), "API Succes", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    showError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RestExerciceImageResponse> call, Throwable t) {
+                showError();
+
+            }
+        });
+
+
+
+    }
+
+    private void showError() {
+        Toast.makeText(getApplicationContext(), "API Error", Toast.LENGTH_SHORT).show();
     }
 }
